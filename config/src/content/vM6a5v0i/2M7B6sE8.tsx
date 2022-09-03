ca22ea42e7f0e0cd3f195bca7299f80ed4d81bd8
Henry Rodrigues <108133435+hhennry@users.noreply.github.com>
2022-09-03 10:14:03 -0300

    <!-- B1.0.4 -->

---
+++ config/src/content/vM6a5v0i/2M7B6sE8.tsx
@@ -0,0 +1,21 @@
+import { MagnifyingGlass } from "phosphor-react";
+import React from "react";
+
+export function C27z7() {
+  return (
+    <div className="">
+      <div className="_Rs9Mg7Q4 flex gap-3 py-2 px-4">
+        <input
+          className=""
+          name="pesquisa"
+          placeholder="Pesquisar"
+          type="search"
+          autoComplete="off"
+        />
+        <button className="_6aLeJ1j1 p-2" title="Pesquisar!" type="submit">
+          <MagnifyingGlass size="22" weight="bold" />
+        </button>
+      </div>
+    </div>
+  );
+}
